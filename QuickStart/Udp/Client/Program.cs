4a688427800d8c848a3cc169c4f925bef18e70f4
devhong <jon.hong@qq.com>
2013-08-20 14:28:29 +0800

    init

---
+++ QuickStart/Udp/Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var udpClient = new System.Net.Sockets.UdpClient();
+            udpClient.Connect(new System.Net.IPEndPoint(System.Net.IPAddress.Parse("127.0.0.1"), 9500));
+            udpClient.Send(new byte[] { 1, 0, 0, 0, 8 }, 5);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ QuickStart/Udp/Server/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new Sodao.FastSocket.Server.UdpServer<MyCmdInfo>(9500, new UdpProtocol(), new UdpService());
+            server.Start();
+            Console.ReadLine();
+        }
+    }
+
+    public class UdpService : Sodao.FastSocket.Server.IUdpService<MyCmdInfo>
+    {
+        public void OnReceived(Sodao.FastSocket.Server.UdpSession session, MyCmdInfo cmdInfo)
+        {
+            Console.WriteLine(cmdInfo.CmdName);
+        }
+
+        public void OnError(Sodao.FastSocket.Server.UdpSession session, Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+
+    public class UdpProtocol : Sodao.FastSocket.Server.Protocol.IUdpProtocol<MyCmdInfo>
+    {
+        public MyCmdInfo FindCommandInfo(ArraySegment<byte> buffer)
+        {
+            //<<len:32-little-endia,cmdName:8,payload:binary>>
+            //len = cmdName.length(1) + payload.length
+            if (buffer.Count < 4) { return null; }
+            var len = BitConverter.ToInt32(buffer.Array, buffer.Offset);
+            if (len < 1) return null;
+            if (buffer.Count < len + 4) return null;
+
+            var payload = new byte[len - 1];
+            Buffer.BlockCopy(buffer.Array, buffer.Offset, payload, 0, payload.Length);
+            return new MyCmdInfo(buffer.Array[buffer.Offset + 4].ToString(), payload);
+        }
+    }
+
+    public class MyCmdInfo : Sodao.FastSocket.Server.Command.ICommandInfo
+    {
+        /// <summary>
+        /// new
+        /// </summary>
+        /// <param name="cmdName"></param>
+        /// <param name="payload"></param>
+        public MyCmdInfo(string cmdName, byte[] payload)
+        {
+            this.CmdName = cmdName;
+            this.Payload = payload;
+        }
+
+        public string CmdName
+        {
+            get;
+            private set;
+        }
+        public byte[] Payload
+        {
+            get;
+            private set;
+        }
+    }
+}
