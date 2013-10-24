a858397d7135d2829e1bfc50820396da73b916d3
devhong <jon.hong@qq.com>
2013-10-24 19:46:55 +0800

    refactoring

---
+++ FastSocket.SocketBase/SendStatus.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Sodao.FastSocket.SocketBase
+{
+    /// <summary>
+    /// packet send status
+    /// </summary>
+    public enum SendStatus : byte
+    {
+        /// <summary>
+        /// 发送成功
+        /// </summary>
+        Success = 1,
+        /// <summary>
+        /// 发送失败
+        /// </summary>
+        Failed = 2
+    }
+}
