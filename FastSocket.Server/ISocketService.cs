8f25d6c3eac584d1ad081af59102b0818d760b51
devhong <jon.hong@qq.com>
2015-03-28 20:30:32 +0800

    init

---
+++ FastSocket.Server/ISocketService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Sodao.FastSocket.Server
+{
+    /// <summary>
+    /// socket service interface.
+    /// </summary>
+    /// <typeparam name="TMessage"></typeparam>
+    public interface ISocketService<TMessage> where TMessage : class, Messaging.IMessage
+    {
+        /// <summary>
+        /// 当建立socket连接时，会调用此方法
+        /// </summary>
+        /// <param name="connection"></param>
+        void OnConnected(SocketBase.IConnection connection);
+        /// <summary>
+        /// 发送回调
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <param name="packet"></param>
+        /// <param name="isSuccess"></param>
+        void OnSendCallback(SocketBase.IConnection connection, SocketBase.Packet packet, bool isSuccess);
+        /// <summary>
+        /// 当接收到客户端新消息时，会调用此方法.
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <param name="message"></param>
+        void OnReceived(SocketBase.IConnection connection, TMessage message);
+        /// <summary>
+        /// 当socket连接断开时，会调用此方法
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <param name="ex"></param>
+        void OnDisconnected(SocketBase.IConnection connection, Exception ex);
+        /// <summary>
+        /// 当发生异常时，会调用此方法
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <param name="ex"></param>
+        void OnException(SocketBase.IConnection connection, Exception ex);
+    }
+}
---
+++ FastSocket.Server/IUdpService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sodao.FastSocket.Server
+{
+    /// <summary>
+    /// udp service interface.
+    /// </summary>
+    /// <typeparam name="TMessage"></typeparam>
+    public interface IUdpService<TMessage> where TMessage : class, Messaging.IMessage
+    {
+        /// <summary>
+        /// on message received
+        /// </summary>
+        /// <param name="session"></param>
+        /// <param name="message"></param>
+        void OnReceived(UdpSession session, TMessage message);
+        /// <summary>
+        /// on error.
+        /// </summary>
+        /// <param name="session"></param>
+        /// <param name="ex"></param>
+        void OnError(UdpSession session, Exception ex);
+    }
+}
