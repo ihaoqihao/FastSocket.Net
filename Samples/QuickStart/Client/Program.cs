980c42dc419db4ef2ed3d7832deb729e0e052f94
devhong <jon.hong@qq.com>
2015-04-02 20:07:44 +0800

    update

---
+++ Samples/QuickStart/Client/Program.cs
@@ -0,0 +1,64 @@
+﻿using Sodao.FastSocket.Client;
+using Sodao.FastSocket.Client.Messaging;
+using Sodao.FastSocket.Client.Protocol;
+using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static private long index = 0;
+
+        static public void Main()
+        {
+            Sodao.FastSocket.SocketBase.Log.Trace.EnableConsole();
+            var client = new SocketClient<CommandLineMessage>(new CommandLineProtocol(), 1024, 1024, 3000, 3000);
+            //建立50个socket连接
+            for (int i = 0; i < 50; i++)
+            {
+                client.TryRegisterEndPoint(i.ToString(), new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1500),
+                    context =>
+                    {
+                        var source = new TaskCompletionSource<bool>();
+                        context.Send(client.NewRequest("init", Encoding.UTF8.GetBytes("init" + System.Environment.NewLine), 3000,
+                            ex => source.TrySetException(ex),
+                            message => source.TrySetResult(true)));
+                        return source.Task;
+                    });
+            }
+
+            //100个同时发送
+            for (int i = 0; i < 100; i++)
+            {
+                Task.Factory.StartNew(() => Do(client));
+            }
+            Console.ReadLine();
+        }
+
+        static private async Task Do(SocketClient<CommandLineMessage> client)
+        {
+            for (int i = 0; i < 100000000; i++)
+            {
+                try { Console.WriteLine(Interlocked.Increment(ref index).ToString() + " " + (await Echo(client)).ToString()); }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(i.ToString() + " " + ex.Message);
+                }
+            }
+        }
+
+        static public Task<bool> Echo(SocketClient<CommandLineMessage> client)
+        {
+            var source = new TaskCompletionSource<bool>();
+            var guid = Guid.NewGuid().ToString();
+            client.Send(client.NewRequest("echo", Encoding.UTF8.GetBytes("echo " + guid + System.Environment.NewLine), 3000,
+                ex => source.TrySetException(ex),
+                message => source.TrySetResult(message.Parameters[0] == guid)));
+            return source.Task;
+        }
+    }
+}
---
+++ Samples/QuickStart/Server/Program.cs
@@ -0,0 +1,72 @@
+﻿using Sodao.FastSocket.Server;
+using Sodao.FastSocket.Server.Messaging;
+using Sodao.FastSocket.SocketBase;
+using System;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SocketServerManager.Init();
+            SocketServerManager.Start();
+
+            //每隔10秒强制断开所有连接
+            System.Threading.Tasks.Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    System.Threading.Thread.Sleep(1000 * 10);
+                    IHost host;
+                    if (SocketServerManager.TryGetHost("quickStart", out host))
+                    {
+                        var arr = host.ListAllConnection();
+                        foreach (var c in arr) c.BeginDisconnect();
+                    }
+                }
+            });
+            Console.ReadLine();
+        }
+    }
+
+    public class MyService : AbsSocketService<CommandLineMessage>
+    {
+        public override void OnConnected(IConnection connection)
+        {
+            base.OnConnected(connection);
+            Console.WriteLine(connection.RemoteEndPoint.ToString() + " connected");
+            connection.BeginReceive();
+        }
+
+        public override void OnReceived(IConnection connection, CommandLineMessage message)
+        {
+            base.OnReceived(connection, message);
+            switch (message.CmdName)
+            {
+                case "echo":
+                    message.Reply(connection, "echo_reply " + message.Parameters[0]);
+                    break;
+                case "init":
+                    Console.WriteLine("connection:" + connection.ConnectionID.ToString() + " init");
+                    message.Reply(connection, "init_reply ok");
+                    break;
+                default:
+                    message.Reply(connection, "error unknow command ");
+                    break;
+            }
+        }
+
+        public override void OnDisconnected(IConnection connection, Exception ex)
+        {
+            base.OnDisconnected(connection, ex);
+            Console.WriteLine(connection.RemoteEndPoint.ToString() + " disconnected");
+        }
+
+        public override void OnException(IConnection connection, Exception ex)
+        {
+            base.OnException(connection, ex);
+            Console.WriteLine(ex.ToString());
+        }
+    }
+}
